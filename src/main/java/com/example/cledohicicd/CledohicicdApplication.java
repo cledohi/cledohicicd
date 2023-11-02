f8aecb160dc356e8b375672824e74b6ae0025826
cledohi <cledohi@gmail.com>
2023-11-02 14:34:32 +0200

    first commit

---
+++ src/main/java/com/example/cledohicicd/CledohicicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.cledohicicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CledohicicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CledohicicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cledohicicd/CledohicicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cledohicicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CledohicicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
