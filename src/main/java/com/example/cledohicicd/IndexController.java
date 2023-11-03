e796767eb30908f82707517f1521c9132921b401
cledohi <cledohi@gmail.com>
2023-11-03 14:26:35 +0200

    add data

--- src/main/java/com/example/cledohicicd/IndexController.java
+++ src/main/java/com/example/cledohicicd/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
     @GetMapping("/")
-    public String index(){
+    public  index(){
         return "welcome app cledohi";
     }
 }
