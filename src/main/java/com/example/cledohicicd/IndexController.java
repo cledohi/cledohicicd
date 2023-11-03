fa0ae3697658e96191eec124f9f8ebe39ded6979
cledohi <cledohi@gmail.com>
2023-11-03 14:42:35 +0200

    [push

--- src/main/java/com/example/cledohicicd/IndexController.java
+++ src/main/java/com/example/cledohicicd/IndexController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
     @GetMapping("/")
     public String index(){
-        dsjksaldsla
         return "welcome app cledohi";
     }
 }
